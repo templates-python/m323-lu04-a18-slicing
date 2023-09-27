1812cafed796faede19ddcdd2b8ee590676819c9
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-27 10:08:40 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+def slicing_special_cases():
+    """Üben von Slicings in Python."""
+
+    # Liste für alle Übungen
+    lst = [1, 2, 3, 4, 5]
+
+    # Extrahiere die ersten drei Elemente der Liste [1, 2, 3, 4, 5].
+    first_three = ...
+
+    # Extrahiere die letzten zwei Elemente der Liste [1, 2, 3, 4, 5].
+    last_two = ...
+
+    # Kehre die Liste [1, 2, 3, 4, 5] um.
+    reversed_lst = ...
+
+    # Extrahiere jeden zweiten Wert der Liste [1, 2, 3, 4, 5].
+    every_second = ...
+
+    # Extrahiere die Elemente zwischen den Indizes -4 und -1 der Liste [1, 2, 3, 4, 5].
+    between_neg4_and_neg1 = ...
+
+    # Extrahiere die ersten drei Elemente der Liste [1, 2, 3, 4, 5] und kehre sie um.
+    first_three_reversed = ...
+
+    # Extrahiere die letzten zwei Elemente der Liste [1, 2, 3, 4, 5] und kehre sie um.
+    last_two_reversed = ...
+
+    # Kehre die Liste [1, 2, 3, 4, 5] um und nimm nur jeden zweiten Wert.
+    reversed_every_second = ...
+
+    # Extrahiere die Elemente zwischen den Indizes -4 und -1 der Liste [1, 2, 3, 4, 5], kehre sie um und nimm nur jeden zweiten Wert.
+    between_neg4_and_neg1_reversed_every_second = ...
+
+    return {
+        'first_three': first_three,
+        'last_two': last_two,
+        'reversed_lst': reversed_lst,
+        'every_second': every_second,
+        'between_neg4_and_neg1': between_neg4_and_neg1,
+        'first_three_reversed': first_three_reversed,
+        'last_two_reversed': last_two_reversed,
+        'reversed_every_second': reversed_every_second,
+        'between_neg4_and_neg1_reversed_every_second': between_neg4_and_neg1_reversed_every_second
+    }
 
 
 if __name__ == '__main__':
-    calculate()
+    print(slicing_special_cases())
--- main_test.py
+++ main_test.py
@@ -1,6 +1,16 @@
-import main
+from main import slicing_special_cases
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+
+def test_slicing_special_cases():
+    result = slicing_special_cases()
+
+    assert result['first_three'] == [1, 2, 3], "Fehler bei den ersten drei Elementen"
+    assert result['last_two'] == [4, 5], "Fehler bei den letzten zwei Elementen"
+    assert result['reversed_lst'] == [5, 4, 3, 2, 1], "Fehler beim Umkehren der Liste"
+    assert result['every_second'] == [1, 3, 5], "Fehler bei jedem zweiten Element"
+    assert result['between_neg4_and_neg1'] == [2, 3, 4], "Fehler zwischen den Indizes -4 und -1"
+    assert result['first_three_reversed'] == [3, 2, 1], "Fehler bei den ersten drei umgekehrten Elementen"
+    assert result['last_two_reversed'] == [5, 4], "Fehler bei den letzten zwei umgekehrten Elementen"
+    assert result['reversed_every_second'] == [5, 3, 1], "Fehler bei jedem zweiten Element der umgekehrten Liste"
+    assert result['between_neg4_and_neg1_reversed_every_second'] == [4,
+        2], "Fehler bei der Verkettung von Slicing-Operationen"
